240729d28fdbef476a9594863ddb0046faf84e11
Mu-An Chiou <me@muanchiou.com>
2019-10-13 17:33:51 -0400

    Exclude duplicated skin tone variant emoji

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const orderedEmojiData = fs.readFileSync('./emoji-order.txt', 'utf-8')
 const groupedEmojiData = fs.readFileSync('./emoji-group.txt', 'utf-8')
 const VARIATION_16 = String.fromCodePoint(0xfe0f)
-const SKIN_TONE_VARIATION_DESC = /\sskin\stone$/
+const SKIN_TONE_VARIATION_DESC = /\sskin\stone(?:,|$)/
 
 // Final data holder
 const orderedEmoji = []
