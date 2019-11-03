fbcd8b2d0f5bdd2504c3ac924658577e7831adea
Mu-An Chiou <me@muanchiou.com>
2019-11-02 20:58:33 -0400

    Allow arg for testing new version

--- script/download-unicode-data.js
+++ script/download-unicode-data.js
@@ -1,13 +1,19 @@
-const currentVersion = '12.1'
+let version = '12.1'
+
+// npm run download 13.0
+if (process.argv[2]) {
+  version = Number(process.argv[2]).toFixed(1).toString()
+}
+
 const fs = require('fs')
 const https = require('https')
 const files = {
   // Complete emoji list with version
-  'emoji-order.txt': `https://unicode.org/emoji/charts-${currentVersion}/emoji-ordering.txt`,
+  'emoji-order.txt': `https://unicode.org/emoji/charts-${version}/emoji-ordering.txt`,
   // Grouped emoji list with qualifier
-  'emoji-group.txt': `https://unicode.org/Public/emoji/${currentVersion}/emoji-test.txt`,
+  'emoji-group.txt': `https://unicode.org/Public/emoji/${version}/emoji-test.txt`,
   // Emoji count
-  'emoji-counts.html': `https://unicode.org/emoji/charts-${currentVersion}/emoji-counts.html`
+  'emoji-counts.html': `https://unicode.org/emoji/charts-${version}/emoji-counts.html`
 }
 
 for (const name of Object.keys(files)) {
