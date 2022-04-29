f110cc765555b9ff774be619a601955854fae5db
Dave Bagler <dave@baglerit.com>
2022-04-29 14:47:19 -0400

    undoing some editor applied styling

--- script/download-unicode-data.js
+++ script/download-unicode-data.js
@@ -1,25 +1,25 @@
-let version = "14.0"
+let version = '14.0'
 
 // npm run download 13.0
 if (process.argv[2]) {
   version = Number(process.argv[2]).toFixed(1).toString()
 }
 
-const fs = require("fs")
-const https = require("https")
+const fs = require('fs')
+const https = require('https')
 const files = {
   // Complete emoji list with version
-  "emoji-order.txt": `https://unicode.org/emoji/charts-${version}/emoji-ordering.txt`,
+  'emoji-order.txt': `https://unicode.org/emoji/charts-${version}/emoji-ordering.txt`,
   // Grouped emoji list with qualifier
-  "emoji-group.txt": `https://unicode.org/Public/emoji/${version}/emoji-test.txt`,
+  'emoji-group.txt': `https://unicode.org/Public/emoji/${version}/emoji-test.txt`,
   // Emoji count
-  "emoji-counts.html": `https://unicode.org/emoji/charts-${version}/emoji-counts.html`
+  'emoji-counts.html': `https://unicode.org/emoji/charts-${version}/emoji-counts.html`
 }
 
 for (const name of Object.keys(files)) {
   const file = fs.createWriteStream(name)
 
-  https.get(files[name], function(response) {
+  const request = https.get(files[name], function(response) {
     response.pipe(file)
   })
 }
