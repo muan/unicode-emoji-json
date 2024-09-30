f761c55bc0519fdaf1d2803cdd4afebe7de7cc61
Mu-An Chiou <github@muan.dev>
2024-09-30 18:57:39 +0800

    Avoid exporting default in case of major version bump

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,22 @@
-declare const dataByEmoji: Record<
-  string,
-  {
+interface Emoji {
     name: string;
     slug: string;
-    group: string;
     emoji_version: string;
     unicode_version: string;
     skin_tone_support: boolean;
-  }
+}
+
+declare const dataByEmoji: Record<
+  string,
+  Emoji & { group: string }
 >;
 
-export default dataByEmoji;
+interface Category {
+  name: string;
+  slug: string;
+  emojis: Array<Emoji & { emoji: string }>
+};
+
+declare const dataByGroup: Array<Category>;
+
+export { dataByEmoji, dataByGroup };
