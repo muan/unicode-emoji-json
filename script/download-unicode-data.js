d97de984728dca734e5013cf8f619065e1ce7c6f
Lakshmi Ananth <gls.ananth4u@gmail.com>
2025-10-12 04:10:58 +0530

    Update download-unicode-data.js
    Updated to version 17

--- script/download-unicode-data.js
+++ script/download-unicode-data.js
@@ -1,6 +1,5 @@
-let version = '15.1'
+let version = '17.0'
 
-// npm run download 13.0
 if (process.argv[2]) {
   version = Number(process.argv[2]).toFixed(1).toString()
 }
@@ -8,17 +7,13 @@ if (process.argv[2]) {
 const fs = require('fs')
 const https = require('https')
 const files = {
-  // Complete emoji list with version
   'emoji-order.txt': `https://unicode.org/emoji/charts-${version}/emoji-ordering.txt`,
-  // Grouped emoji list with qualifier
   'emoji-group.txt': `https://unicode.org/Public/emoji/${version}/emoji-test.txt`,
-  // Emoji count
   'emoji-counts.html': `https://unicode.org/emoji/charts-${version}/emoji-counts.html`
 }
 
 for (const name of Object.keys(files)) {
   const file = fs.createWriteStream(name)
-
   https.get(files[name], function(response) {
     response.pipe(file)
   })
